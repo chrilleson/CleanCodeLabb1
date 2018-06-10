34c5fd1220b34a6b55beb92f4d1a10fec08d47b0
Christoffer Ljungqvist <christoffer.ljungqvist@hotmail.se>
2018-06-10 23:33:26 +0200

    Projects created

---
+++ CleanCodeLabb1/CleanCodeLabb1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanCodeLabb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
