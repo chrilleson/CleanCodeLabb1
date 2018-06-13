d722fb3816865025e11d15aa5eb3ae4c134e0fa2
Christoffer Ljungqvist <christoffer.ljungqvist@hotmail.se>
2018-06-13 22:03:42 +0200

    Testfalls for all

--- CleanCodeLabb1/XUnitTestCleanCodeLabb1/UnitTest1.cs
+++ CleanCodeLabb1/XUnitTestCleanCodeLabb1/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace XUnitTestCleanCodeLabb1
             account.Deposit(deposit);
             double actualBalance = account.Balance;
             double expectedBalance = 250.00;
-            Assert.Equal(actualBalance, expectedBalance);
+            Assert.Equal(expectedBalance, actualBalance);
         }
 
         [Fact]
@@ -59,6 +59,171 @@ namespace XUnitTestCleanCodeLabb1
             Assert.Throws<Exception>(() =>
             {
                 account.Deposit(double.NegativeInfinity);
+            });
+        }
+
+        [Fact]
+        public void WithdrawDecreaseBalance()
+        {
+            Account account = new Account(200.00);
+
+            double withdraw = 50.00;
+            account.Withdraw(withdraw);
+            double actualBalnace = account.Balance;
+            double expectedBalance = 150.00;
+
+            Assert.Equal(expectedBalance, actualBalnace);
+        }
+
+        [Fact]
+        public void WithdrawShouldThrowIfUnavailableFunds()
+        {
+            Account account = new Account(200.00);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(250.00);
+            });
+        }
+
+        [Fact]
+        public void WithdrawShouldThrowIfAmountIsNegative()
+        {
+            Account account = new Account(200.00);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(-50.00);
+            });
+        }
+
+        [Fact]
+        public void WithdrawShouldThrowIfNaN()
+        {
+            Account account = new Account(200.00);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(double.NaN);
+            });
+        }
+
+        [Fact]
+        public void WithdrawShouldThrowIfPositiveInfinity()
+        {
+            Account account = new Account(200.00);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(double.PositiveInfinity);
+            });
+        }
+
+        [Fact]
+        public void WithdrawShouldThrowIfNegativeInfinity()
+        {
+            Account account = new Account(200.00);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Withdraw(double.NegativeInfinity);
+            });
+        }
+
+        [Fact]
+        public void TransferShouldTransferToSavingsIfSuccessful()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(1000);
+
+            double amount = 100;
+            account.Transfer(savingsAccount, amount);
+            double actualBalance = savingsAccount.Balance;
+            double expectedBalance = 1100;
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+
+        [Fact]
+        public void TransferShouldWithdrawFromAccountIfSuccessful()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(1000);
+
+            double amount = 100;
+            account.Transfer(savingsAccount, amount);
+            double actualBalance = account.Balance;
+            double expectedBalance = 100;
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+
+        [Fact]
+        public void TransferShouldThrowIfAmountToTransferIsNegative()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(50);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(savingsAccount, -1);
+            });
+        }
+
+        [Fact]
+        public void TransferShouldThrowIfTransferingToSameAccount()
+        {
+            Account account = new Account(200.00);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(account, 5);
+            });
+        }
+
+        [Fact]
+        public void TransferShouldThrowIfNull()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(50);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(null, 0);
+            });
+        }
+
+        [Fact]
+        public void TransferShouldThrowIfNaN()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(50);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(savingsAccount, double.NaN);
+            });
+        }
+
+        [Fact]
+        public void TransferShouldThrowIfPositiveInfinity()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(50);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(savingsAccount, double.PositiveInfinity);
+            });
+        }
+
+        [Fact]
+        public void TransferShouldThrowIfNegativeInfinity()
+        {
+            Account account = new Account(200.00);
+            Account savingsAccount = new Account(50);
+
+            Assert.Throws<Exception>(() =>
+            {
+                account.Transfer(savingsAccount, double.NegativeInfinity);
             });
         }
     }
